020dc0ea0283295b4845875caebf3f2920ef33bb
kirby <kirby.dpx@proton.me>
2026-09-02 02:34:53 +0200

    .

--- Makefile
+++ Makefile
@@ -14,30 +14,49 @@ GO_FLAGS := CGO_ENABLED=1
 
 LIBSIGNAL_DIR := deps/libsignal
 LIBSIGNAL_VERSION := v0.101.2
-LIBSIGNAL_FFI_H := deps/libsignal_ffi.h
+LIBSIGNAL_FFI_CBINDGEN := libsignal/rust/bridge/ffi/cbindgen.toml
+LIBSIGNAL_FFI_H := deps/signal_ffi.h
 LIBSIGNAL_FFI_A := deps/libsignal_ffi.a
+
+# rust version = nightly-2026-07-16
+# protoc?
 
 
 .PHONY: all
-all: build
+all: clean build
 
 $(LIBSIGNAL_DIR):
-	git submodule init && git submodule update
-	cd $(LIBSIGNAL_DIR) && git checkout $(LIBSIGNAL_VERSION)
+	@echo "[$(LIBSIGNAL_DIR)] ..."
+	#git submodule init && git submodule update
+	#cd $(LIBSIGNAL_DIR) && git checkout $(LIBSIGNAL_VERSION)
+	# checkout submodules recursive
 
+#cargo +${{ matrix.toolchain }} build --workspace --features libsignal-ffi/signal-media --verbose --keep-going
 $(LIBSIGNAL_FFI_A): $(LIBSIGNAL_DIR)
+	@echo "[$(LIBSIGNAL_FFI_A)] ..."
 	cd $(LIBSIGNAL_DIR) && RUSTFLAGS="$(CARGO_RUSTFLAGS)" $(CARGO_BIN) build -p libsignal-ffi --release
 	@cp -v $(LIBSIGNAL_DIR)/target/release/libsignal_ffi.a $(LIBSIGNAL_FFI_A)
 
+#deps/libsignal/swift/Sources/SignalFfi/signal_ffi.h
+# doas sysctl hardening.harden_rtld=0
+# swift/build_ffi.sh --release --generate-ffi
 $(LIBSIGNAL_FFI_H): $(LIBSIGNAL_DIR)
-	echo skipping cbindgen
-	#$(CBINDGEN_BIN) --config $(CBINDGEN_CFG) --profile release --crate libsignal-ffi --output $(LIBSIGNAL_FFI_H) --lang c
+	#$(CBINDGEN_BIN)  --profile release --crate "$(LIBSIGNAL_DIR)"/rust/bridge/ffi --output $(LIBSIGNAL_FFI_H) --lang c
+	#$(CBINDGEN_BIN) --config $(CBINDGEN_CFG) --profile release "$(LIBSIGNAL_DIR)"/rust/bridge/ffi --output $(LIBSIGNAL_FFI_H) --lang c
+
+	#  libsignal/rust/bridge/ffi
+	#cd $(LIBSIGNAL_DIR)/rust/bridge/ffi && $(CBINDGEN_BIN) --config $(LIBSIGNAL_FFI_CBINDGEN) --profile release --output $(LIBSIGNAL_FFI_H) --lang c
+	@echo "[$(LIBSIGNAL_FFI_H)] ..."
+	cp -nv deps/libsignal/swift/Sources/SignalFfi/*.h deps/
 
 build: $(LIBSIGNAL_FFI_A) $(LIBSIGNAL_FFI_H) test
+	@echo "[Building] ..."
 	$(GO_FLAGS) $(GO_BIN) build -v .
 
 test: $(LIBSIGNAL_FFI_A) $(LIBSIGNAL_FFI_H)
+	@echo "[Testing] ..."
 	$(GO_FLAGS) $(GO_BIN) test -v .
 
 clean:
-	@echo "clean?"
+	@echo "[Cleaning] ..."
+	@rm -fv libsignal-go
--- main.go
+++ main.go
@@ -1,85 +1,10 @@
 package main
 
 /*
-#cgo LDFLAGS: ./libsignal_ffi.a
+#include "./deps/signal_ffi.h"
+#include "./deps/signal_ffi_testing.h"
+#cgo LDFLAGS: ./deps/libsignal_ffi.a
 */
 import "C"
 
-// https://github.com/ihciah/rust2go
-
 func main() {}
-
-/*
-[workspace]
-# When adding new members, consider updating the log filter in rust/bridge/shared/src/logging.rs.
-members = [
-    "rust/attest",
-    "rust/crypto",
-    "rust/debug",
-    "rust/device-transfer",
-    "rust/keytrans",
-    "rust/media",
-    "rust/message-backup",
-    "rust/net",
-    "rust/net/chat",
-    "rust/net/infra",
-    "rust/account-keys",
-    "rust/poksho",
-    "rust/protocol",
-    "rust/usernames",
-    "rust/zkcredential",
-    "rust/zkgroup",
-    "rust/bridge/ffi",
-    "rust/bridge/ffi/impl",
-    "rust/bridge/ffi/native_swift",
-    "rust/bridge/jni",
-    "rust/bridge/jni/impl",
-    "rust/bridge/jni/native_kt",
-    "rust/bridge/jni/testing",
-    "rust/bridge/node",
-    "rust/bridge/node/native_ts",
-]
-default-members = [
-    "rust/crypto",
-    "rust/device-transfer",
-    "rust/media",
-    "rust/message-backup",
-    "rust/account-keys",
-    "rust/poksho",
-    "rust/protocol",
-    "rust/usernames",
-    "rust/zkcredential",
-    "rust/zkgroup",
-]
-
-[workspace.dependencies]
-# Our own crates, so that we don't have to depend on them by inter-crate paths
-attest = { path = "rust/attest" }
-device-transfer = { path = "rust/device-transfer" }
-libsignal-account-keys = { path = "rust/account-keys" }
-libsignal-cli-utils = { path = "rust/cli-utils" }
-libsignal-core = { path = "rust/core" }
-libsignal-debug = { path = "rust/debug" }
-libsignal-keytrans = { path = "rust/keytrans" }
-libsignal-message-backup = { path = "rust/message-backup" }
-libsignal-net = { path = "rust/net" }
-libsignal-net-chat = { path = "rust/net/chat" }
-libsignal-net-grpc = { path = "rust/net/grpc" }
-libsignal-node = { path = "rust/bridge/node" }
-libsignal-protocol = { path = "rust/protocol" }
-libsignal-svr2 = { path = "rust/svr2" }
-libsignal-svrb = { path = "rust/svrb" }
-poksho = { path = "rust/poksho" }
-signal-crypto = { path = "rust/crypto" }
-signal-media = { path = "rust/media" }
-usernames = { path = "rust/usernames" }
-zkcredential = { path = "rust/zkcredential" }
-zkgroup = { path = "rust/zkgroup" }
-
-libsignal-bridge = { path = "rust/bridge/shared" }
-libsignal-bridge-macros = { path = "rust/bridge/shared/macros" }
-libsignal-bridge-testing = { path = "rust/bridge/shared/testing" }
-libsignal-bridge-types = { path = "rust/bridge/shared/types" }
-libsignal-jni-impl = { path = "rust/bridge/jni/impl" }
-signal-neon-futures = { path = "rust/bridge/node/futures" }
-*/
